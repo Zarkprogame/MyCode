aa1313ab5ccb35f6c427985cf5adbcb165caa117
zarkprogame <zarkprogame@gmail.com>
2022-11-21 17:25:13 -0500

    Ex1 al 100

--- JAVA/Ex1/src/App.java
+++ JAVA/Ex1/src/App.java
@@ -1,20 +1,25 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
         int user_number, multi;
+        String vector[] = new String[5];
+        String mostrar = "Primeras 10 posiciones en la tabla de multiplicar de tu numero: \n";
+        user_number = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un Numero: "));
 
-        Scanner teclado = new Scanner(System.in);
-        
-        System.out.print("Ingrese un Numero: ");
-        user_number = teclado.nextInt();
-
-        System.out.println("Primeras 10 posiciones en la tabla de multiplicar del numero: " + user_number + ": ");
-
-        for (int counter = 1; counter <= 10; counter ++){
+        for (int counter = 1; counter < 11; counter++){
             multi = counter * user_number;
-            System.out.println(counter + " * " + user_number + " = " + multi);
+            vector[0] = String.valueOf(counter);
+            vector[1] = " * ";
+            vector[2] = String.valueOf(user_number);
+            vector[3] = " = ";
+            vector[4] = String.valueOf(multi);
+
+            for (int i = 0; i < 5; i++){
+                mostrar = mostrar + vector[i];
+            }
+            mostrar += "\n";
         }
-        
+        JOptionPane.showMessageDialog(null, mostrar);
     }
 }
