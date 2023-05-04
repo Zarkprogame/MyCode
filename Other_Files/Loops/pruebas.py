3075e33151ed4e1e937eeffa4c00affed84bcde8
zarkprogame <zarkprogame@gmail.com>
2023-05-04 15:21:02 -0500

    Organizacion de todas las carpetas

--- Other_Files/Loops/pruebas.py
+++
@@ -1,6 +0,0 @@
-
-print("Elija 1 si desea hacer una suma")
-print("Elija 1 si desea hacer una resta")
-print("Elija 1 si desea hacer una multiplicacion")
-print("Elija 1 si desea hacer una division")
-
